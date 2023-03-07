30439213cb0b7710b600e416aa17a8539ab53a41
swastik <swastikgorai@gmail.com>
2023-03-07 21:12:07 +0530

    Smoothed parallax effect

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ let gateRight=document.getElementById('gate-right');
 window.addEventListener('scroll',()=>{
     let value= window.scrollY;
 
-    text.style.marginTop= value * 2.5 +'px';
-    treeLeft.style.left=value* -1.5 + 'px';
-    treeRight.style.left= value* 1.5 + 'px';
-    gateLeft.style.left=value* 0.5 + 'px';
-    gateRight.style.left= value* -0.5 + 'px';
+    text.style.marginTop= value * 1.3 +'px';
+    treeLeft.style.left=value* -0.125 + 'px';
+    treeRight.style.left= value* 0.125 + 'px';
+    gateLeft.style.left=value* 0.135 + 'px';
+    gateRight.style.left= value* -0.135 + 'px';
 })
